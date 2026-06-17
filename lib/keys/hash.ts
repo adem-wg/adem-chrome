412935adc21fcf331cb8f919f319c22aa67d4988
Felix Linker <linkerfelix@gmail.com>
2026-06-17 11:04:05 +0200

    Remove base32 implementation

--- lib/keys/hash.ts
+++ lib/keys/hash.ts
@@ -1,33 +1,7 @@
 import { calculateJwkThumbprint, JWK } from 'jose';
-
-const BASE32 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-
-function base32(input: Uint8Array): string {
-  let bits = 0;
-  let value = 0;
-  let output = '';
-  for (const byte of input) {
-    value = (value << 8) | byte;
-    bits += 8;
-    while (bits >= 5) {
-      output += BASE32[(value >>> (bits - 5)) & 31];
-      bits -= 5;
-    }
-  }
-  if (bits > 0) {
-    output += BASE32[(value << (5 - bits)) & 31];
-  }
-  return output.toLowerCase();
-}
+import { base32 } from 'rfc4648';
 
 export async function calculateKid(jwk: JWK): Promise<string> {
   const digest = await calculateJwkThumbprint(jwk, 'sha256');
-  return base32(Buffer.from(digest, 'base64url'));
-}
-
-export function readKID(jwk?: JWK): string {
-  if (jwk?.kid === undefined) {
-    throw new Error('no kid present');
-  }
-  return jwk.kid;
+  return base32.stringify(Buffer.from(digest, 'base64url'), { pad: false }).toLowerCase();
 }
